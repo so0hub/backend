121c1764d67e17e91bbe012f6d561673324d3a16
박소영 <so0mile02@gmail.com>
2026-02-04 16:20:36 +0900

    260204 day10 Exam2

---
+++ src/day10/키보드.java
@@ -0,0 +1,13 @@
+package day10;
+
+public interface 키보드 {
+
+    // 상수 , public static final 생략 가능
+    public static final String 제조사 = "인텔";
+    String 제조일 = "2026-02-24";
+
+    // 추상메소드 , public abstract 생략 가능
+    public abstract void akey();
+    int bkey(int x , int y);
+
+}
